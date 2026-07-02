e7fecb9830f7112dc76670a527e8a3a2d290426f
kevinbarrios22 <keanbago@gmail.com>
2026-07-02 22:49:43 +0200

    add the test

--- src/test/java/com/leaf/api_leaf/ApiLeafApplicationTests.java
+++ src/test/java/com/leaf/api_leaf/ApiLeafApplicationTests.java
@@ -1,13 +1,14 @@
 package com.leaf.api_leaf;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Tag("integration")
 @SpringBootTest
 class ApiLeafApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
